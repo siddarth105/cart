ac5e2bc1d95987e3892b752489a4490f82fb80c6
Siddarth <siddharth.at@target.com>
2018-05-18 23:42:18 +0530

    change2

---
+++ src/main/java/com/ecomm/chkt/cart/repository/domain/Order.java
@@ -0,0 +1,61 @@
+package com.ecomm.chkt.cart.repository.domain;
+
+import com.datastax.driver.mapping.annotations.ClusteringColumn;
+import com.datastax.driver.mapping.annotations.Column;
+import com.datastax.driver.mapping.annotations.PartitionKey;
+import com.datastax.driver.mapping.annotations.Table;
+
+@Table(name = "orders")
+public class Order {
+
+    @PartitionKey
+    @Column(name="o_order_id")
+    private int orderId;
+    @Column(name="o_order_status")
+    private String status;
+
+    @ClusteringColumn
+    @Column(name="oi_order_item_id")
+    private int itemId;
+    @Column(name="oi_order_item_name")
+    private String itemName;
+
+    public Order(int orderId, String status, int itemId, String itemName) {
+        this.orderId = orderId;
+        this.status = status;
+        this.itemId = itemId;
+        this.itemName = itemName;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+}
